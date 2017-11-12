f929ab1850b8b2977802c4c3312f84b67d329e25
Joseph Moloughney <jmoloughney25@gmail.com>
2017-11-12 23:59:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_hash = {}
+  data.each do |key, value|
+    value.each do |kv, array|
+      array.each do |name|
+        if organized_hash[name] == nil
+          organized_hash[name] = {}
+          end
+          if organized_hash[name][key] == nil
+            organized_hash[name][key] = []
+          end
+          organized_hash[name][key].push(kv.to_s)
+        end
+      end
+    end
+  return organized_hash
 end
